b6f592e29c3388008cd13645a3d4e818fb5a42c4
Thai Thanh Hai <thaithanhhaick@gmail.com>
2025-07-06 21:15:28 +0700

    Revert "feat: implement CRUD budget"
    This reverts commit f9a09b3665f97e92bbd3e619d182dcf48769a8b7.

--- src/main/java/com/learn/expense_tracker/budget/BudgetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.learn.expense_tracker.budget;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BudgetRepository extends JpaRepository<Budget,Long> {
-
-}
