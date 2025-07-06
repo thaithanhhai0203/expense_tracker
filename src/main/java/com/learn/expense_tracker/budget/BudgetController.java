dc9b56f7e366f808ccd396243e894e449cd9ea5a
hongquang1412 <hongquangst456@gmail.com>
2025-07-06 22:28:11 +0700

    feat: implement CRUD category

--- src/main/java/com/learn/expense_tracker/budget/BudgetController.java
+++ src/main/java/com/learn/expense_tracker/budget/BudgetController.java
@@ -2,6 +2,7 @@ package com.learn.expense_tracker.budget;
 
 import com.learn.expense_tracker.budget.request.BudgetRequest;
 import com.learn.expense_tracker.budget.response.BudgetResponse;
+import com.learn.expense_tracker.category.response.CategoryResponse;
 import com.learn.expense_tracker.common.dto.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -28,10 +29,30 @@ public class BudgetController {
         return this.budgetService.getAll();
     }
 
+    @Operation(summary = "Get budget detail")
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<BudgetResponse>> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(ApiResponse.success(this.budgetService.getById(id)));
+    }
+
     @Operation(summary = "Create budget")
     @PostMapping
     public ResponseEntity<ApiResponse<String>> create(@RequestBody BudgetRequest budgetRequest,  @RequestHeader("Authorization") String authHeader) {
         String token = authHeader.replace("Bearer ", "");
-        return ResponseEntity.ok(ApiResponse.success(this.budgetService.save(budgetRequest, token), "Created budget"));
+        return ResponseEntity.ok(ApiResponse.success(this.budgetService.save(budgetRequest, token)));
+    }
+
+    @Operation(summary = "Update budget")
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<String>> update(@PathVariable Long id, @RequestBody BudgetRequest budgetRequest, @RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "");
+       return ResponseEntity.ok(ApiResponse.success(this.budgetService.update(id, budgetRequest, token)));
+    }
+
+    @Operation(summary = "Delete budget")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<String>> delete(@PathVariable Long id, @RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "");
+        return ResponseEntity.ok(ApiResponse.success(this.budgetService.delete(id, token)));
     }
 }
