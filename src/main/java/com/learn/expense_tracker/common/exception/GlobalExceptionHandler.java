5c4aaea76f624d57fbacdc6916f86977986ae77f
Thai Thanh Hai <thaithanhhaick@gmail.com>
2025-07-06 21:28:37 +0700

    clean: fix conflict code

--- src/main/java/com/learn/expense_tracker/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/learn/expense_tracker/common/exception/GlobalExceptionHandler.java
@@ -1,10 +1,9 @@
 package com.learn.expense_tracker.common.exception;
 
 import com.learn.expense_tracker.common.dto.ApiResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
